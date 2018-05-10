7e8c7c126d1ef741fa2077d21871ed8334752923
skokul <skokul@users.noreply.github.com>
2018-05-10 22:20:24 +0530

    Limit

--- ﻿﻿9-Givenarangeie2numebrslandrcountthenumberofprimenumbersintherange.py
+++ ﻿﻿9-Givenarangeie2numebrslandrcountthenumberofprimenumbersintherange.py
@@ -2,13 +2,16 @@ j=int(input())
 i=int(input())
 k=0
 count=0
-for t in range(j,i+1):
-	for inn in range(2,t+1):
-		if(t%inn==0):
-			k=k+1
-	if(k==1):
-		count=count+1
-		k=0
-	else:
-		k=0
-print(count)
+if(j<=100000 and i<=100000):
+	for t in range(j,i+1):
+		for inn in range(2,t+1):
+			if(t%inn==0):
+				k=k+1
+		if(k==1):
+			count=count+1
+			k=0
+		else:
+			k=0
+	print(count)
+else:
+	print(0)
