2ae67c4b1a6aef5f52b3fb1b391f2660bfc8ec0e
skokul <skokul@users.noreply.github.com>
2018-05-10 22:42:55 +0530

    Test

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
 j=int(input())
-i=int(input())
-k=0
-count=0
-for t in range(j,i+1):
-	for inn in range(2,t+1):
-		if(t%inn==0):
-			k=k+1
-	if(k==1):
-		count=count+1
-		k=0
-	else:
-		k=0
-print(count)
+a=[]
+t=""
+if(1 <= j <=100000):
+	for i in range(j):
+		a.append(int(input()))
+a.sort()
+for i in range(j):
+	t=t+str(a[i])
+print(t[::-1])
