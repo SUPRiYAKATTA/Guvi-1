e555e4ecdc6028da8ca9befe0c54d03187978287
skokul <skokul@users.noreply.github.com>
2018-05-12 07:31:05 +0530

    Input size condition added

--- Hunter/﻿1-GivenanumberNfollowedbyNnumbers.OutoftheseNnumberssomeofthemarerepeated.Writeaprogramtofindthenumberswhicharerepeatedandprintthenumbersonceinsortedorder.py
+++ Hunter/﻿1-GivenanumberNfollowedbyNnumbers.OutoftheseNnumberssomeofthemarerepeated.Writeaprogramtofindthenumberswhicharerepeatedandprintthenumbersonceinsortedorder.py
@@ -1,7 +1,8 @@
 size=int(input())
-a=[]
-for i in range(size):
-	a.append(int(input()))
-for i in set(a):
-	a.remove(i)
-print(sorted(a))
+	if 0<size<100000:
+	a=[]
+	for i in range(size):
+		a.append(int(input()))
+	for i in set(a):
+		a.remove(i)
+	print(sorted(a))
