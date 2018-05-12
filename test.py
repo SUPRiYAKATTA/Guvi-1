234becdcb255abf1ecabb4234a95bd9dbfd3e5bc
skokul <skokul@users.noreply.github.com>
2018-05-12 07:55:55 +0530

    Indentation

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
 size=int(input())
 if 0<size<100000:
-		a=[]
-		j=0
-		b=[]
-		for i in range(size):
-			a.append(int(input()))
-		for i in a:
-			if j==i:
-				b.append(j)
-				j=j+1
-			else:
-				j=j+1
-		if len(b)<1:
-			print(-1)
-		else:
-			print(sorted(b))
+	a=[]
+	b=[]
+	for i in range(size):
+		a.append(int(input()))
+	for i in set(a):
+		a.remove(i)
+	if len(a)<1:
+		print("Unique")
+	else:
+		print(a[0])
